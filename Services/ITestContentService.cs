380c1f1de4865ba001d3c0f24376164a73dd795a
Albert Hajdu <albert.hajdu@lombiq.com>
2017-06-29 14:56:00 +0200

    Adding interface documentation
    --HG--
    branch : issue/ORCH-26

--- Services/ITestContentService.cs
+++ Services/ITestContentService.cs
@@ -9,11 +9,29 @@ using System.Web;
 
 namespace Lombiq.OrchardContentStressTest.Services
 {
+    /// <summary>
+    /// Service for managing test contents.
+    /// </summary>
     public interface ITestContentService : IDependency
     {
+        /// <summary>
+        /// Gets the test blog created in the migration.
+        /// </summary>
         IContent GetTestBlog();
+
+        /// <summary>
+        /// Gets the test blog post created in the migration.
+        /// </summary>
         IContent GetTestBlogPost();
+
+        /// <summary>
+        /// Gets the test images from the file system.
+        /// </summary>
         FileInfo[] GetTestImages();
+
+        /// <summary>
+        /// Gets the test taxonomy created in the migration.
+        /// </summary>
         TaxonomyPart GetTestTaxonomy();
     }
 }
