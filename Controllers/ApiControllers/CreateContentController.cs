7f1f9e018c4442586d8c7b05a3d834aeb03a411f
Albert Hajdu <albert.hajdu@lombiq.com>
2017-06-29 13:52:32 +0200

    Namespace code stylings
    --HG--
    branch : issue/ORCH-26

--- Controllers/ApiControllers/CreateContentController.cs
+++ Controllers/ApiControllers/CreateContentController.cs
@@ -7,7 +7,7 @@ using Orchard.ContentPicker.Fields;
 using Orchard.Core.Common.Fields;
 using Orchard.Core.Title.Models;
 using Orchard.Fields.Fields;
-using Orchard.Layouts.Elements;
+using LayoutElements = Orchard.Layouts.Elements;
 using Orchard.Layouts.Models;
 using Orchard.Layouts.Services;
 using Orchard.Localization;
@@ -196,80 +196,80 @@ namespace Lombiq.OrchardContentStressTest.Controllers.ApiControllers
         }
 
 
-        private void SetTitlePart(Orchard.ContentManagement.ContentItem contentItem)
+        private void SetTitlePart(ContentItem contentItem)
         {
             contentItem.As<TitlePart>().Title = _faker.Lorem.Sentence();
         }
 
-        private void SetBodyPart(Orchard.ContentManagement.ContentItem contentItem)
+        private void SetBodyPart(ContentItem contentItem)
         {
             contentItem.As<BodyPart>().Text = _faker.Lorem.Paragraphs();
         }
 
-        private void SetLayoutPart(Orchard.ContentManagement.ContentItem contentItem)
+        private void SetLayoutPart(ContentItem contentItem)
         {
-            var canvas = _elementManager.ActivateElement<Canvas>();
-            var html = _elementManager.ActivateElement<Html>();
+            var canvas = _elementManager.ActivateElement<LayoutElements.Canvas>();
+            var html = _elementManager.ActivateElement<LayoutElements.Html>();
             canvas.Elements.Add(html);
             html.Content = _faker.Lorem.Paragraphs();
             contentItem.As<LayoutPart>().LayoutData = _layoutSerializer.Serialize(new[] { canvas });
         }
 
-        private void SetTagsPart(Orchard.ContentManagement.ContentItem contentItem)
+        private void SetTagsPart(ContentItem contentItem)
         {
             contentItem.As<TagsPart>().CurrentTags =
                 _faker.Commerce.Categories(10).Select(category => _tagService.CreateTag(category).TagName);
         }
 
-        private void SetContainer(Orchard.ContentManagement.ContentItem contentItem, IContent container)
+        private void SetContainer(ContentItem contentItem, IContent container)
         {
             contentItem.As<CommonPart>().Container = container;
         }
 
-        private void SetBooleanField(Orchard.ContentManagement.ContentItem contentItem, string partName, string fieldName)
+        private void SetBooleanField(ContentItem contentItem, string partName, string fieldName)
         {
             contentItem.AsField<BooleanField>(partName, fieldName).Value = _faker.Random.Bool();
         }
 
-        private void SetContentPickerField(Orchard.ContentManagement.ContentItem contentItem, string partName, string fieldName)
+        private void SetContentPickerField(ContentItem contentItem, string partName, string fieldName)
         {
             contentItem.AsField<ContentPickerField>(partName, fieldName).Ids = new int[] { _faker.Random.Number(2, 100) };
         }
 
-        private void SetDateTimeField(Orchard.ContentManagement.ContentItem contentItem, string partName, string fieldName)
+        private void SetDateTimeField(ContentItem contentItem, string partName, string fieldName)
         {
             contentItem.AsField<DateTimeField>(partName, fieldName).DateTime = _faker.Date.Future();
         }
 
-        private void SetEnumerationField(Orchard.ContentManagement.ContentItem contentItem, string partName, string fieldName)
+        private void SetEnumerationField(ContentItem contentItem, string partName, string fieldName)
         {
             var field = contentItem.AsField<EnumerationField>(partName, fieldName);
             var settings = field.PartFieldDefinition.Settings.GetModel<EnumerationFieldSettings>();
             field.SelectedValues = new string[] { settings.Options.Split(new string[] { Environment.NewLine }, StringSplitOptions.None)[_faker.Random.Number(0, Config.TestEnumerationFieldOptionsNumber - 1)] };
         }
 
-        private void SetInputField(Orchard.ContentManagement.ContentItem contentItem, string partName, string fieldName)
+        private void SetInputField(ContentItem contentItem, string partName, string fieldName)
         {
             contentItem.AsField<InputField>(partName, fieldName).Value = _faker.Lorem.Sentence();
         }
 
-        private void SetLinkField(Orchard.ContentManagement.ContentItem contentItem, string partName, string fieldName)
+        private void SetLinkField(ContentItem contentItem, string partName, string fieldName)
         {
             contentItem.AsField<LinkField>(partName, fieldName).Value = _faker.Internet.DomainName();
         }
 
-        private void SetMediaLibraryPickerField(Orchard.ContentManagement.ContentItem contentItem, string partName, string fieldName)
+        private void SetMediaLibraryPickerField(ContentItem contentItem, string partName, string fieldName)
         {
             var images = _contentManager.Query("Image").Slice(0, 100);
             contentItem.AsField<MediaLibraryPickerField>(partName, fieldName).Ids = new int[] { images.ElementAt(_faker.Random.Number(images.Count() - 1)).Id };
         }
 
-        private void SetNumericField(Orchard.ContentManagement.ContentItem contentItem, string partName, string fieldName)
+        private void SetNumericField(ContentItem contentItem, string partName, string fieldName)
         {
             contentItem.AsField<NumericField>(partName, fieldName).Value = _faker.Random.Number(0, 100);
         }
 
-        private void SetTextField(Orchard.ContentManagement.ContentItem contentItem, string partName, string fieldName)
+        private void SetTextField(ContentItem contentItem, string partName, string fieldName)
         {
             contentItem.AsField<TextField>(partName, fieldName).Value = _faker.Lorem.Word();
         }
